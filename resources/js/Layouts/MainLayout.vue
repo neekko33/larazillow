bc3f25f767522cb9ec5d9406026c5a7dd517aa8d
neekko33 <neekko33@gmail.com>
2025-11-19 17:34:11 +0800

    add styles for main layout page

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -8,16 +8,30 @@ const flashMessage = computed(() => page.props.flash.success);
 </script>
 
 <template>
-    <div class="p-4">
-        <Link :href="route('listing.index')" class="mr-2">Listing</Link>
-        <Link :href="route('listing.create')">New Listing</Link>
+    <div>
+        <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800">
+            <div class="container mx-auto">
+                <nav class="p-4 flex items-center justify-between">
+                    <div class="text-lg font-medium">
+                        <Link :href="route('listing.index')" class="mr-2">Listings</Link>
+                    </div>
+                    <div class="text-lg font-bold text-indigo-600 dark:text-indigo-400">
+                        <Link :href="route('listing.index')" class="mr-2">LaraZillow</Link>
+                    </div>
+                    <div>
+                        <Link :href="route('listing.create')"
+                            class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+                    </div>
+                </nav>
+            </div>
+        </header>
 
-        <div v-if="flashMessage" class="text-green-500 my-4">
-            {{ flashMessage }}
-        </div>
-
-        <div class="mt-4">
+        <main class="container mx-auto p-4">
+            <div v-if="flashMessage" class="my-4 text-white font-medium bg-green-500 px-4 py-2 rounded-md shadow-sm">
+                {{ flashMessage }}
+            </div>
             <slot />
-        </div>
+        </main>
+
     </div>
 </template>
